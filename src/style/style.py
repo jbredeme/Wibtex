60dcb2b473d1a601138b95d9ded8b9436e47fbb4
Charlie Duso <dusocharles@hotmail.com>
2017-04-02 18:07:14 -0700

    Added function to split tags into a list of lists - separated by bibliography tag

--- src/style/style.py
+++ src/style/style.py
@@ -15,15 +15,43 @@ def read_style_file( file ):
 def validate_syntax( doc_list ):
     regexp = re.compile(r'\\bibliography\{(.*?)\}')
     
-    # Ensure that last item is bibliography tag
+    # Ensure that last item is bibliography tag - TODO may be revised
     if not regexp.search(doc_list[(len(doc_list) - 1)]):
         print("Error: '\\bibliography{}' should be final tag in document.")
 
-    # Validate that a bibliography tag exists
+    # Validate that a bibliography tag exists - TODO may be unnecessary
     for item in range(0, len(doc_list)):
         if regexp.search(doc_list[item]):
             print(doc_list[item])
     return
+
+def split_citations( doc_list ):
+
+    regexp = re.compile(r'\\bibliography\{(.*?)\}')
+    bib_count = 0
+    num_list  = []
+
+    # Count the number of bibliography tags
+    for item in range(0, len(doc_list)):
+        if regexp.search(doc_list[item]):
+            num_list.append(item)
+            bib_count += 1
+
+    # Create a list or list of lists
+    if bib_count > 1:
+        bib_list  = []
+        temp_list = []
+        for item in range(0, len(doc_list)):
+            if item in num_list:
+                temp_list.append(doc_list[item])
+                bib_list.append(temp_list)
+                temp_list = []
+            else:
+                temp_list.append(doc_list[item])
+    else:
+        bib_list = doc_list
+
+    return bib_list
 
 def extract_citations( doc_list ):
     #todo
@@ -56,9 +84,3 @@ def generate_works_cited( citations ):
 
 def get_reference_data( style_file, doc_list ):
     return
-
-test = ['\\cite{demo}', '\\cite{demo2}', '\\cite{demo3}', '\\bibliography{demo4}']
-#print(test)
-
-#print(strip_markup(test))
-validate_syntax(test)
