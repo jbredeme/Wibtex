90d41ac296f57ea851e44af2e6c5d2de63c6eb0a
Charlie Duso <dusocharles@hotmail.com>
2017-04-02 17:47:08 -0700

    Added syntax validation function
    Function checks that bibliography tag is in list and last item should be
    bibliography tag

--- src/style/style.py
+++ src/style/style.py
@@ -12,12 +12,32 @@ def read_style_file( file ):
 
 # Reference Data Generation
 
-def strip_markup( citations ):
-    for item in range(0, len(citations)):
-        citations[item] = re.sub(r'\\cite\{', '', citations[item])
-        citations[item] = re.sub(r'\\bibliography\{', '', citations[item])
-        citations[item] = re.sub(r'\}', '', citations[item])
-    return citations
+def validate_syntax( doc_list ):
+    regexp = re.compile(r'\\bibliography\{(.*?)\}')
+    
+    # Ensure that last item is bibliography tag
+    if not regexp.search(doc_list[(len(doc_list) - 1)]):
+        print("Error: '\\bibliography{}' should be final tag in document.")
+
+    # Validate that a bibliography tag exists
+    for item in range(0, len(doc_list)):
+        if regexp.search(doc_list[item]):
+            print(doc_list[item])
+    return
+
+def extract_citations( doc_list ):
+    #todo
+    return
+
+def extract_bibliography( doc_list ):
+    return
+
+def strip_markup( doc_list ):
+    for item in range(0, len(doc_list)):
+        doc_list[item] = re.sub(r'\\cite\{', '', doc_list[item])
+        doc_list[item] = re.sub(r'\\bibliography\{', '', doc_list[item])
+        doc_list[item] = re.sub(r'\}', '', doc_list[item])
+    return doc_list
 
 def validate_citations( citations ):
     #todo
@@ -27,6 +47,18 @@ def organize_citations( citations ):
     #todo
     return
 
-def generate_references( citations ):
+def generate_citations( citations ):
     #todo
     return
+
+def generate_works_cited( citations ):
+    return
+
+def get_reference_data( style_file, doc_list ):
+    return
+
+test = ['\\cite{demo}', '\\cite{demo2}', '\\cite{demo3}', '\\bibliography{demo4}']
+#print(test)
+
+#print(strip_markup(test))
+validate_syntax(test)
