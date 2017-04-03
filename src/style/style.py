57b4ad55f55aa5836b19ae3c3318bdd27e50d2da
Charlie Duso <dusocharles@hotmail.com>
2017-04-02 19:13:05 -0700

    Added strip markup function

--- src/style/style.py
+++ src/style/style.py
@@ -12,17 +12,19 @@ def read_style_file( file ):
 
 # Reference Data Generation
 
-def validate_syntax( doc_list ):
+def validate_syntax( bib_list ):
     regexp = re.compile(r'\\bibliography\{(.*?)\}')
     
-    # Ensure that last item is bibliography tag - TODO may be revised
-    if not regexp.search(doc_list[(len(doc_list) - 1)]):
+    # Ensure that last item is bibliography tag - TODO may be dropped
+    if not regexp.search(bib_list[(len(bib_list) - 1)]):
         print("Error: '\\bibliography{}' should be final tag in document.")
 
-    # Validate that a bibliography tag exists - TODO may be unnecessary
-    for item in range(0, len(doc_list)):
-        if regexp.search(doc_list[item]):
-            print(doc_list[item])
+    # Validate that a bibliography tag exists
+    valid = False
+    for item in range(0, len(bib_list)):
+        if regexp.search(bib_list[item]):
+            valid = True
+
     return
 
 def split_citations( doc_list ):
@@ -54,26 +56,40 @@ def split_citations( doc_list ):
     return bib_list
 
 def extract_citations( doc_list ):
-    #todo
+    #TODO - May not be needed
     return
 
 def extract_bibliography( doc_list ):
+    #TODO - May not be needed
     return
 
 def strip_markup( doc_list ):
-    for item in range(0, len(doc_list)):
-        doc_list[item] = re.sub(r'\\cite\{', '', doc_list[item])
-        doc_list[item] = re.sub(r'\\bibliography\{', '', doc_list[item])
-        doc_list[item] = re.sub(r'\}', '', doc_list[item])
+
+    if any(isinstance(el, list) for el in doc_list):
+        print("List of lists")
+        for sub_list in doc_list:
+            for item in range(0, len(sub_list)):
+                sub_list[item] = re.sub(r'\\cite\{', '', sub_list[item])
+                sub_list[item] = re.sub(r'\\bibliography\{', '', sub_list[item])
+                sub_list[item] = re.sub(r'\}', '', sub_list[item])
+    else:
+        print("Single list")
+        for item in range(0, len(doc_list)):
+            doc_list[item] = re.sub(r'\\cite\{', '', doc_list[item])
+            doc_list[item] = re.sub(r'\\bibliography\{', '', doc_list[item])
+            doc_list[item] = re.sub(r'\}', '', doc_list[item])
+
     return doc_list
 
 def validate_citations( citations ):
     #todo
     return
 
-def organize_citations( citations ):
-    #todo
-    return
+def organize_citations( citations, alpha = False ):
+    if alpha:
+        temp_list = citations[0:(len(citations) - 1)]
+
+    return citations
 
 def generate_citations( citations ):
     #todo
