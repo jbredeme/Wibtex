7dc69c3bcda2a0053947c4adef30be45ec0c4d08
Charlie Duso <dusocharles@hotmail.com>
2017-04-02 17:04:57 -0700

    Code skeleton

---
+++ src/style/style.py
@@ -0,0 +1,27 @@
+# Style File Extraction
+
+def validate_style_file( file ):
+    #todo
+    return
+
+def read_style_file( file ):
+    #todo
+    return
+
+# Reference Data Generation
+
+def strip_markup( citations ):
+    #todo
+    return
+
+def validate_citations( citations ):
+    #todo
+    return
+
+def organize_citations( citations ):
+    #todo
+    return
+
+def generate_references( citations ):
+    #todo
+    return
