3e84c25dadb83d04dcdbf3c4526af83499aca4ce
Charlie Duso <dusocharles@hotmail.com>
2017-04-05 21:40:28 -0700

    Added more structuring for execution file

--- src/main.py
+++ src/main.py
@@ -12,28 +12,40 @@
 ################################################
 import style
 import wibtex_parser
-import docx_io
+#import docx_io
+
+################################################
+# Construct Input Data
+################################################
+input_bib   = "test_data/demo_bib.bib"
+input_doc   = "test_data/demo.docx"
+input_style = "test_data/demo_style.json"
+
+bib_data    = {}
+cite_data   = {}
 
 ################################################
 # Read BibTeX Database (wibtex_parser.py)
 ################################################
 
-# TODO
+bib_data = wibtex_parser.parse(input_bib)
 
 ################################################
 # Read Word Document (docx_io.py)
 ################################################
 
-# TODO
+# TODO - Jarid's Code goes here
 
 ################################################
 # Generate Reference Data (style.py)
 ################################################
 
-# TODO
+cite_data = get_reference_data( style_file, style, doc_list, bib_data )
 
 ################################################
 # Write to Word Document (docx_io.py)
 ################################################
 
-# TODO
+# TODO - Jarid's Code goes here
+
+# DONE :)
