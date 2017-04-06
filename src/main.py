1b4e3dcdd753f41293c6651013f21c79cca6fbcc
Charlie Duso <dusocharles@hotmail.com>
2017-04-05 20:53:59 -0700

    Added primary execution file skeleton

---
+++ src/main.py
@@ -0,0 +1,39 @@
+############################################################
+# @file   main.py
+# @brief  module to execute the WibTeX Reference Management
+#         System
+#
+# @author Hayden Aupperle, Jarid Bredemeier, Charles Duso
+# @date   April 5, 2017
+############################################################
+
+################################################
+# Import Python Modules
+################################################
+import style
+import wibtex_parser
+import docx_io
+
+################################################
+# Read BibTeX Database (wibtex_parser.py)
+################################################
+
+# TODO
+
+################################################
+# Read Word Document (docx_io.py)
+################################################
+
+# TODO
+
+################################################
+# Generate Reference Data (style.py)
+################################################
+
+# TODO
+
+################################################
+# Write to Word Document (docx_io.py)
+################################################
+
+# TODO
